cff359e074c7920c60f5e03997e71afc069625a3
mttgermano <mttgermano@proton.me>
2024-11-20 00:07:47 -0300

    fix: 008

--- submission/008.sh
+++ submission/008.sh
@@ -2,6 +2,6 @@
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
 
 txid_input0="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
-public_key=$(bitcoin-cli getrawtransaction "$txid_input0" 1 | jq -r '.vin[0].txinwitness[2]')
+public_key=$(bitcoin-cli getrawtransaction "$txid_input0" 1 | jq -r '.vin[0].txinwitness[0]')
 
 echo $public_key
