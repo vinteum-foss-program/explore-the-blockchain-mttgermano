5cfe84d7a704a1c2a8197aa43520bf66ac5137ae
mttgermano <mttgermano@proton.me>
2024-11-20 00:00:04 -0300

    solve 1,2,3,4,6,8

--- submission/006.sh
+++ submission/006.sh
@@ -1,1 +1,17 @@
 # Which tx in block 257,343 spends the coinbase output of block 256,128?
+
+block2_id=256128
+block1_id=257343
+
+coinbase_txid=$(bitcoin-cli getblockhash $block2_id | xargs -I{} bitcoin-cli getblock {} | jq -r '.tx[0]')
+block=$(bitcoin-cli getblockhash $block1_id | xargs -I{} bitcoin-cli getblock {} | jq -r '.tx[]')
+spender=""
+for txid in $block; do
+  inputs=$(bitcoin-cli getrawtransaction "$txid" 1 | jq -r '.vin[].txid')
+  if [[ $inputs == *"$coinbase_txid"* ]]; then
+    spender=$txid
+    break
+  fi
+done
+
+echo $spender
--- submission/008.sh
+++ submission/008.sh
@@ -1,2 +1,7 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+
+txid_input0="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
+public_key=$(bitcoin-cli getrawtransaction "$txid_input0" 1 | jq -r '.vin[0].txinwitness[2]')
+
+echo $public_key
